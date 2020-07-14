1deae939f0050ced7c07409b8a367504b6d3c93c
sawaseem123 <axs31050@ucmo.edu>
2020-07-14 11:55:17 -0500

    my portfolio website

---
+++ server1.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def page(page_name):
+    return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'Did not save to database'
+    else:
+        return "something went wrong..!"
+
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as db2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(db2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+# def write_database(data):
+#     with open('database.txt', mode='a') as db:
+#         email = data['email']
+#         subject = data['subject']
+#         message = data['message']
+#         file = db.write(f'\n{email},{subject},{message}')
+
+# @app.route('/about.html')
+# def about():
+#     return render_template('about.html',name=about)
+#
+#
+# @app.route('/contact.html')
+# def contact():
+#     return render_template('contact.html')
